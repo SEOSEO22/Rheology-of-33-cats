11b52e3543931d56d9889c53dd9d5c46156afcaa
Kayoung <exopcy0506@naver.com>
2025-05-12 14:25:16 +0900

    UI_1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //확인용입니다. 이 게임메니저는 삭제해주세요!
+    public static GameManager Instance;
+
+    public int currentStageNum; // 현재 스테이지
+    public int stageCatCount;   // 현재 스테이지에서 찾은 고양이 수
+ 
+}
